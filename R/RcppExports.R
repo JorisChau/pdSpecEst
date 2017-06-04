6066ef0a69a3f6e5183a3636d530efb399b1e863
JorisChau <joris.chau1@gmail.com>
2017-06-04 16:30:32 +0200

    Test commit linux

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -106,23 +106,6 @@ NormF <- function(M) {
     .Call(pdSpecEst_NormF, M)
 }
 
-#' Riemannian distance HPD-matrices
-#'
-#' \code{RiemmDist} calculates the natural Riemannian distance between two Hermitian PD matrices as
-#' in (Bhatia, 2009, Chapter 6).
-#'
-#' @param A,B Hermitian positive-definite matrices (of equal dimension).
-#'
-#' @examples
-#'  a <- matrix(complex(real = rnorm(9), imaginary = rnorm(9)), nrow = 3)
-#'  A <- t(Conj(a)) %*% a
-#'  b <- matrix(complex(real = rnorm(9), imaginary = rnorm(9)), nrow = 3)
-#'  B <- t(Conj(b)) %*% b
-#'  RiemmDist(A, B)
-#'
-#' @references Bhatia, R. (2009). \emph{Positive Definite Matrices}. New Jersey: Princeton University Press.
-#'
-#' @export
 RiemmDist <- function(A, B) {
     .Call(pdSpecEst_RiemmDist, A, B)
 }
--- R/dist.R
+++ R/dist.R
@@ -1,9 +1,34 @@
-#' Various distance measures between HPD matrices
+#' Compute distance between two HPD matrices
+#'
+#' \code{pdDist} calculates the user-specified distance between two Hermitian PD matrices.
+#'
+#' Available distance functions between two Hermitian PD matrices are (i) Riemannian distance (default) as in
+#' (Bhatia, 2009, Chapter 6), (ii) log-Euclidean distance, the Euclidean distance between matrix logarithms,
+#' (iii) Cholesky distance, the Euclidean distance between Cholesky decompositions, (iv) Euclidean distance,
+#' and (v) Procrustes distance as in (Dryden et al., 2009). In particular, \code{pdDist} generalizes the function
+#' \code{\link[shapes]{distCov}} computing the distance between two symmetric positive-definite matrices.
+#'
+#' @param A,B Hermitian positive-definite matrices (of equal dimension).
+#' @param method the distance measure, one of \code{'Riemannian'},
+#' \code{'logEuclidean'}, \code{'Cholesky'}, \code{'Euclidean'} or \code{'Procrustes'}. Defaults to \code{'Riemannian'}.
+#'
+#' @examples
+#'  a <- matrix(complex(real = rnorm(9), imaginary = rnorm(9)), nrow = 3)
+#'  A <- t(Conj(a)) %*% a
+#'  b <- matrix(complex(real = rnorm(9), imaginary = rnorm(9)), nrow = 3)
+#'  B <- t(Conj(b)) %*% b
+#'  pdDist(A, B) ## By default the Riemannian distance is computed
+#'
+#' @references Bhatia, R. (2009). \emph{Positive Definite Matrices}. New Jersey: Princeton University Press.
+#' @references Dryden, I.L., Koloydenko, A., Zhou, D. (2009). Non-Euclidean statistics for covariance matrices,
+#' with applications to diffusion tensor imaging. \emph{Annals of Applied Statistics}, 3(3), 1102-1123.
+#'
+#' @seealso \code{\link[shapes]{distCov}}
 #'
 #' @export
 pdDist <- function(A, B, method = 'Riemannian'){
 
-  if (!(is.TRUE(all.equal(dim(Ad), dim(B)) & (length(dim(A)) == 3)))) {
+  if (!(isTRUE(all.equal(dim(A), dim(B)) & (length(dim(A)) == 3)))) {
     stop("Incorrect input dimensions for arguments: 'A' and/or 'B',
              consult the function documentation for the requested inputs.")
   }
@@ -27,6 +52,6 @@ pdDist <- function(A, B, method = 'Riemannian'){
     l2 <- Sqrt(B)
     dd <- sqrt(NormF(l1)^2 + NormF(l2)^2 - 2 * sum(svd(t(Conj(l2)) %*% l1)$d))
   }
-  dd
+  return(dd)
 }
 
